021aa744ccdcc3c16ff9c80fc272eae7bb311c9f
SBEGENJOV <seydi19981995@gmail.com>
2023-12-26 04:23:01 +0300

    Categori listeleme silme ve güncelleme işlemleri bitti

--- routes/category/categoryRouter.js
+++ routes/category/categoryRouter.js
@@ -1,9 +1,20 @@
 const express = require("express");
-const { createCategory } = require("../../controllers/categories/category");
+const {
+  createCategory,
+  getCategories,
+  deleteCategory,
+  updateCategory,
+} = require("../../controllers/categories/category");
 const isLoggin = require("../../middlewares/isLoggin");
 
 const categoryRouter = express.Router();
 
 // oluşturma
 categoryRouter.post("/", isLoggin, createCategory);
+// hepsini getir
+categoryRouter.get("/", getCategories);
+//Silme
+categoryRouter.delete("/:id", isLoggin, deleteCategory);
+//Güncelleme
+categoryRouter.put("/:id", isLoggin, updateCategory);
 module.exports = categoryRouter;
